a1f61f3f0fa151b6d21d8a927764bf2a248d68ff
Mohamed Arsath <90707804+arsath-13@users.noreply.github.com>
2024-01-14 13:26:39 +0530

    ESH-001 : Dependencies Added

---
+++ SwapIt_BackEnd/src/main/java/com/SwapIT/SwapIt_BackEnd/SwapItBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.SwapIT.SwapIt_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapItBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapItBackEndApplication.class, args);
+	}
+
+}
---
+++ SwapIt_BackEnd/src/test/java/com/SwapIT/SwapIt_BackEnd/SwapItBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SwapIT.SwapIt_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapItBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
