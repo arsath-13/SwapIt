0ead6c21677e5c1fb7158b56c678ce170038c88a
Mohamed Arsath <90707804+arsath-13@users.noreply.github.com>
2024-01-29 04:06:17 +0530

    ESH-002 : Angular Guard and Role Based Navigation facilities added

---
+++ SwapIt_FrontEnd/src/app/components/login/login.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup,FormControl, Validators } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AuthService } from '../../services/auth-service/auth.service';
+import { Route, Router } from '@angular/router';
+import { LocalStorageService } from '../../services/storage-service/local-storage.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  validateForm!:FormGroup;
+
+  constructor (private authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router) {  }
+
+  ngOnInit() {
+    this.validateForm=this.fb.group({
+      username:[null,[Validators.required]],
+      password:[null,[Validators.required]]
+    })
+  }
+
+  login() {
+    console.log(this.validateForm);
+    this.authService.login(this.validateForm.get(['username'])!.value,this.validateForm.get(['password'])!.value).subscribe((res:any) =>{
+      console.log(res);
+      
+    //   if (LocalStorageService.isAdminLoggedIn()) {
+    //     this.router.navigateByUrl("/admin/dashboard");
+    //   }else if(LocalStorageService.isUserLoggedIn()) {
+    //     this.router.navigateByUrl("/user/dashboard");
+    //   }
+    // }, (error: any)=> {
+    //   console.log(error)
+    //   if(error.status == 406) {
+    //     alert("ERROR! \n Account is not active. Please Register!!!")
+    //   }
+    //   else {
+    //     alert("Error! \n Bad Credentials!!!")
+    //   }
+    const role = res.body.role;
+
+      if (role === 'ADMIN') {
+        this.router.navigateByUrl("/admin/dashboard");
+      } else if (role === 'USER') {
+        this.router.navigateByUrl("/user/dashboard");
+      }
+    },
+    (error: any) => {
+      console.log(error);
+
+      if (error.status == 406) {
+        alert("ERROR! \n Account is not active. Please Register!!!");
+      } else {
+        alert("Error! \n Bad Credentials!!!");
+      }
+    }
+    
+    )
+     }
+}
