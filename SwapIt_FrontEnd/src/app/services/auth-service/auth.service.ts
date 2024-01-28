843476ad2d53895c4af54b46641303e48e019159
Mohamed Arsath <90707804+arsath-13@users.noreply.github.com>
2024-01-28 11:27:32 +0530

    ESH-002 : Register Facility added

--- SwapIt_FrontEnd/src/app/services/auth-service/auth.service.ts
+++ SwapIt_FrontEnd/src/app/services/auth-service/auth.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environments';
+
+const BASIC_URL = environment["BASIC_URL"]
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  register(signupDTO:any):Observable<any> {
+    return this.http.post(BASIC_URL + "sign-up", signupDTO)
+  }
 }
